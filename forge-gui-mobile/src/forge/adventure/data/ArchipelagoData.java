4a1969c4d91ae1fca5e1a050c6864e8e3309d9f3
Bram Teurlings <bram.teurlings@live.nl>
2026-02-07 12:14:52 +0100

    Added real ways for players to unlock things in the game Todo: Refactor some of the unlocks to a separate file because this one is getting crowded.

--- forge-gui-mobile/src/forge/adventure/data/ArchipelagoData.java
+++ forge-gui-mobile/src/forge/adventure/data/ArchipelagoData.java
@@ -16,13 +16,12 @@ import java.util.*;
 public class ArchipelagoData implements SaveFileContent {
     private static ArchipelagoData instance = null;
 
-    // Ask Forge for the full list of all sets
+    // Data we need from Forge
     private final CardEdition.Collection allEditions = StaticData.instance().getEditions();
     private final Iterable<CardEdition> allOrderedEditions = allEditions.getOrderedEditions();
     // Todo: This works fine for singleplayer even when updates come out but the fact that the list of all sets can grow will cause problems in Archipelago due to a variable amount of checks.
     private final Set<String> allCardSets = new HashSet<>();
     // List of teleportation runes that we use to gate regions
-    // Todo: Move this and other static locations/checks/values to their own file.
     private final Set<String> regionTeleportingRunes = new HashSet<>(Arrays.asList("White rune","Black rune","Blue rune","Red rune","Green rune"));
 
     // Actual user data we want to store
@@ -39,11 +38,19 @@ public class ArchipelagoData implements SaveFileContent {
     private int totalGoldEarned = 0;
     private int totalExtraMaxLifeEarned = 0;
     private int totalShardsEarned = 0;
+    private int totalBattlesWon = 0;
+
     // List of unlockable checks
     // Todo: Fill list based on archipelago xml contents
-    // Todo: Implement logic to divide the total amount of sets by the amountOfSetUnlockChecks and give the player an amount of checks equal to the result. Also keep track of the rest amount by using a float for the next check.
-    private final int amountOfSetUnlockChecks = 100;
+    private int receivedAmountOfSetUnlockChecks = 0;
+    private float setUnlockChecksRestAmount = 0;
     private final Set<String> listOfUnlockableItems = new HashSet<>();
+    private final int totalAmountOfSetUnlockChecks = 100;
+    private final int totalBattlesWonBreakpoint = 3; // Reward for every 3 battles won.
+    private final int totalTownQuestsAndEventsBreakpoint = 4; // Reward for every 4 town events or quests done.
+    private final int totalCardsEarnedBreakPoint = 130; // Reward for every 130 town events or quests done.
+
+    private enum ARCHIPELAGO_CHECK_TYPES {BATTLES_WON, TOWN_QUESTS_AND_EVENTS_DONE, TOTAL_CARDS_EARNED};
 
     public ArchipelagoData() {
         instance = this;
@@ -51,6 +58,45 @@ public class ArchipelagoData implements SaveFileContent {
 
     public static ArchipelagoData getInstance() {
         return instance == null ? instance = new ArchipelagoData() : instance;
+    }
+
+    // Todo: Add more checks for other things the player can do such as earn gold, shards, defeating bosses etc.
+    private void updatePlayerChecks(ARCHIPELAGO_CHECK_TYPES type) {
+        switch (type) {
+            case BATTLES_WON -> {
+                if (totalBattlesWon > 0 && totalBattlesWon % totalBattlesWonBreakpoint == 0) {
+                    unlockRandomSet();
+                }
+            }
+            case TOWN_QUESTS_AND_EVENTS_DONE -> {
+                int totalTownQuestsAndEventsDone = completedTownInnEvents.size() + completedTownQuests.size();
+                if (totalTownQuestsAndEventsDone > 0 && totalTownQuestsAndEventsDone % totalTownQuestsAndEventsBreakpoint == 0) {
+                    unlockRandomRegion();
+                }
+            }
+            case TOTAL_CARDS_EARNED -> {
+                long totalCardsEarned = 0;
+                for (long value : cardsEarnedByRarity.values()) {
+                    totalCardsEarned += value;
+                }
+                if (totalCardsEarned > 0 && totalCardsEarned % totalCardsEarnedBreakPoint == 0) {
+                    unlockRandomSet();
+                }
+            }
+        }
+    }
+
+    private void unlockSetByName(String setToUnlock) {
+        addSetUnlockedByCode(setToUnlock);
+        String setUnlockedText = "FORGE_ARCHIPELAGO: CARD SET REWARD: " + setToUnlock;
+        // Some sets don't have booster packs such as full-art land sets (P23).
+        var booster = StaticData.instance().getBoosters().get(setToUnlock);
+        if (booster != null) {
+            Current.player().addBooster(AdventureEventController.instance().generateBooster(setToUnlock));
+            setUnlockedText = "FORGE_ARCHIPELAGO: CARD SET REWARD + BOOSTER DETECTED: " + setToUnlock;
+        }
+        System.out.println(setUnlockedText);
+        GameHUD.getInstance().addNotification(setUnlockedText, 0.5f, 3f, 0.5f);
     }
 
     public void unlockRandomSet() {
@@ -63,33 +109,37 @@ public class ArchipelagoData implements SaveFileContent {
             return;
         }
 
-        // Todo: After picking a random set, check if boosters can be generated from this set, if not, add it to the unlocked sets and generate another one.
-        // Pick a random locked set
-        int index = new Random().nextInt(lockedSets.size());
-        String setToUnlock = null;
-
-        int i = 0;
-        for (String set : lockedSets) {
-            if (i++ == index) {
-                setToUnlock = set;
-                break;
+        // If we already received all checks, unlock the entire list of locked sets
+        if (receivedAmountOfSetUnlockChecks >= totalAmountOfSetUnlockChecks) {
+            for (String set : lockedSets) {
+                unlockSetByName(set);
             }
         }
 
-        if (setToUnlock != null) {
-            addSetUnlockedByCode(setToUnlock);
-            String setUnlockedText = "FORGE_ARCHIPELAGO: CARD SET REWARD: " + setToUnlock;
-            // Some sets don't have booster packs such as full-art land sets (P23).
-            var booster = StaticData.instance().getBoosters().get(setToUnlock);
-            if (booster != null) {
-                Current.player().addBooster(AdventureEventController.instance().generateBooster(setToUnlock));
-                setUnlockedText = "FORGE_ARCHIPELAGO: CARD SET REWARD + BOOSTER DETECTED: " + setToUnlock;
+        float amountOfSetsToUnlock = (float) allCardSets.size() / totalAmountOfSetUnlockChecks + setUnlockChecksRestAmount;
+        int amountOfSetsToUnlockFloored = (int) Math.floor(amountOfSetsToUnlock);
+        setUnlockChecksRestAmount = (amountOfSetsToUnlock - amountOfSetsToUnlockFloored);
+        for (int i = 0; i < amountOfSetsToUnlockFloored; i++) {
+            int targetSetIndex = new Random().nextInt(lockedSets.size());
+            String setToUnlock = null;
+
+            int setIndex = 0;
+            for (String set : lockedSets) {
+                if (setIndex++ == targetSetIndex) {
+                    setToUnlock = set;
+                    break;
+                }
             }
-            System.out.println(setUnlockedText);
-            GameHUD.getInstance().addNotification(setUnlockedText, 0.5f, 3f, 0.5f);
+
+            if (setToUnlock != null) {
+                unlockSetByName(setToUnlock);
+                lockedSets.remove(setToUnlock);
+            }
         }
+        receivedAmountOfSetUnlockChecks++;
     }
 
+    // Todo: Rename "biome" to "region" after all current PRs are merged.
     public boolean isBiomeUnlocked(String regionName) {
         if (lockedWorldRegionsByName.contains(regionName)) {
             return false;
@@ -97,15 +147,35 @@ public class ArchipelagoData implements SaveFileContent {
         return true;
     }
 
+    // Keep this updated to reset any sets/maps/variables
     public void setupFreshSaveFile() {
+        cardsUnlockedByName.clear();
         this.addCardUnlockedByName("Plains");
         this.addCardUnlockedByName("Forest");
         this.addCardUnlockedByName("Swamp");
         this.addCardUnlockedByName("Mountain");
         this.addCardUnlockedByName("Island");
         this.addCardUnlockedByName("Wastes");
+
+        completedTownInnEvents.clear();
+        completedTownQuests.clear();
+        cardsEarnedByRarity.clear();
+        itemsGainedByName.clear();
+        packsEarnedBySet.clear();
+
+        setsUnlockedByCode.clear();
+        bossesDefeatedByName.clear();
+        miniBossesDefeatedByName.clear();
         lockedWorldRegionsByName.clear();
         lockedWorldRegionsByName.addAll(new HashSet<>(Arrays.asList("white","blue","black","red","green")));
+
+        totalGoldEarned = 0;
+        totalExtraMaxLifeEarned = 0;
+        totalShardsEarned = 0;
+        totalBattlesWon = 0;
+
+        receivedAmountOfSetUnlockChecks = 0;
+        setUnlockChecksRestAmount = 0f;
     }
 
     public boolean checkCardUnlocked(PaperCard card) {
@@ -145,29 +215,25 @@ public class ArchipelagoData implements SaveFileContent {
         String townName = TileMapScene.instance().rootPoint.getDisplayName();
         completedTownInnEvents.merge(townName, 1L, Long::sum);
         System.out.println("FORGE_ARCHIPELAGO: INN EVENT COMPLETION DETECTED: " + townName + " - " + completedTownInnEvents.get(townName));
+        updatePlayerChecks(ARCHIPELAGO_CHECK_TYPES.TOWN_QUESTS_AND_EVENTS_DONE);
     }
 
     public void addCompletedQuests(AdventureQuestEvent event) {
         String townName = event.poi.getDisplayName();
         completedTownQuests.merge(townName, 1L, Long::sum);
         System.out.println("FORGE_ARCHIPELAGO: QUEST COMPLETION DETECTED: " + townName + " - " + completedTownQuests.get(townName));
+        updatePlayerChecks(ARCHIPELAGO_CHECK_TYPES.TOWN_QUESTS_AND_EVENTS_DONE);
     }
 
     public void addCardByRarity(String rarity) {
         cardsEarnedByRarity.merge(rarity, 1L, Long::sum);
-
-        // Todo: Temporary unlock of new set when player gains a card, remove later
-        unlockRandomSet();
-        unlockRegionByName("white");
-        unlockRegionByName("blue");
-        unlockRegionByName("black");
-        unlockRegionByName("red");
-        unlockRegionByName("green");
+        // Todo: This method will be called a lot when we receive a large batch of cards, make sure this doesn't cause too much slowdown.
+        updatePlayerChecks(ARCHIPELAGO_CHECK_TYPES.TOTAL_CARDS_EARNED);
     }
 
     public void addGold(int amount) {
         totalGoldEarned += amount;
-        System.out.println("FORGE_ARCHIPELAGO: ITEM REWARD DETECTED: " + amount);
+        System.out.println("FORGE_ARCHIPELAGO: GOLD REWARD DETECTED: " + amount);
     }
 
     // Due to MapDialog.SetEffects() using just a name string to add items to the player's inventory, it's likely that the name is unique.
@@ -186,7 +252,9 @@ public class ArchipelagoData implements SaveFileContent {
             } else if (itemName.toLowerCase().contains("green")) {
                 unlockRegionByName("green");
             }
-            System.out.println("FORGE_ARCHIPELAGO: REGION REWARD DETECTED: " + itemName);
+            String regionUnlockMessage = "FORGE_ARCHIPELAGO: REGION REWARD DETECTED: " + itemName;
+            System.out.println(regionUnlockMessage);
+            GameHUD.getInstance().addNotification(regionUnlockMessage, 0.5f, 3f, 0.5f);
         }
         itemsGainedByName.merge(itemName, 1L, Long::sum);
         System.out.println("FORGE_ARCHIPELAGO: ITEM REWARD DETECTED: " + itemName);
@@ -207,6 +275,11 @@ public class ArchipelagoData implements SaveFileContent {
         System.out.println("FORGE_ARCHIPELAGO: SHARD REWARD DETECTED: +" + amount);
     }
 
+    public void addTotalBattlesWon(int amount) {
+        totalBattlesWon += amount;
+        updatePlayerChecks(ARCHIPELAGO_CHECK_TYPES.BATTLES_WON);
+    }
+
     // Note that the name of a boss is not unique so we'll need to filter from all enemies which have a `boss` value of `true`.
     // Returns `true` if the boss was not already defeated before.
     public boolean addMiniBossDefeated(String miniBossName) {
@@ -224,6 +297,25 @@ public class ArchipelagoData implements SaveFileContent {
     }
     public void unlockRegionByName(String regionName) {
         lockedWorldRegionsByName.remove(regionName);
+    }
+
+    public void unlockRandomRegion() {
+        if (lockedWorldRegionsByName.isEmpty()) {
+            return;
+        }
+
+        int targetRegionIndex = new Random().nextInt(lockedWorldRegionsByName.size());
+        int setIndex = 0;
+        for (String region : lockedWorldRegionsByName) {
+            if (setIndex++ == targetRegionIndex) {
+                for (String runeName : regionTeleportingRunes) {
+                    if (runeName.toLowerCase().contains(region.toLowerCase())) {
+                        Current.player().addItem(runeName);
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     // Helper functions for saving and loading
@@ -272,6 +364,7 @@ public class ArchipelagoData implements SaveFileContent {
     @Override
     public void load(SaveFileData data) {
         if (data == null) {
+            setupFreshSaveFile();
             return;
         }
 
@@ -296,6 +389,9 @@ public class ArchipelagoData implements SaveFileContent {
         loadStringSet(data, "setsUnlocked", setsUnlockedByCode);
         loadStringSet(data, "lockedRegions", lockedWorldRegionsByName);
 
+        setUnlockChecksRestAmount = data.containsKey("setUnlocksReceivedRest") ? data.readInt("setUnlocksReceivedRest") : 0;
+        receivedAmountOfSetUnlockChecks = data.containsKey("setUnlocksReceived") ? data.readInt("setUnlocksReceived") : 0;
+        totalBattlesWon = data.containsKey("totalBattlesWon") ? data.readInt("totalBattlesWon") : 0;
         totalGoldEarned = data.containsKey("totalGold") ? data.readInt("totalGold") : 0;
         totalExtraMaxLifeEarned = data.containsKey("extraLife") ? data.readInt("extraLife") : 0;
         totalShardsEarned = data.containsKey("shards") ? data.readInt("shards") : 0;
@@ -316,6 +412,9 @@ public class ArchipelagoData implements SaveFileContent {
         saveStringSet(data, "setsUnlocked", setsUnlockedByCode);
         saveStringSet(data, "lockedRegions", lockedWorldRegionsByName);
 
+        data.store("setUnlocksReceivedRest", setUnlockChecksRestAmount);
+        data.store("setUnlocksReceived", receivedAmountOfSetUnlockChecks);
+        data.store("totalBattlesWon", totalBattlesWon);
         data.store("totalGold", totalGoldEarned);
         data.store("extraLife", totalExtraMaxLifeEarned);
         data.store("shards", totalShardsEarned);
